370238a4a2930b04f03585b04dddd52074053f57
Maxeg13 <chibisaincrada@gmail.com>
2018-01-04 17:58:06 +0300

    dialog extended

--- dialog.cpp
+++ dialog.cpp
@@ -81,3 +81,63 @@ Dialog::~Dialog()
 {
 
 }
+
+void Dialog::drawingInit(QwtPlot* d_plot, QString title)
+{
+
+    //        setCentralWidget(MW);
+
+    //canvas().resize(925,342)
+    //    d_plot->canvas()->resize(100,150);
+    //d_plot->autoRefresh();
+    d_plot->setAutoReplot();
+    //_______232
+
+    // настройка функций
+    QwtPlotPicker *d_picker =
+            new QwtPlotPicker(
+                QwtPlot::xBottom, QwtPlot::yLeft, // ассоциация с осями
+                QwtPlotPicker::CrossRubberBand, // стиль перпендикулярных линий
+                QwtPicker::ActiveOnly, // включение/выключение
+                d_plot->canvas() ); // ассоциация с полем
+    // Цвет перпендикулярных линий
+    d_picker->setRubberBandPen( QColor( Qt::red ) );
+
+    // цвет координат положения указателя
+    d_picker->setTrackerPen( QColor( Qt::black ) );
+
+    // непосредственное включение вышеописанных функций
+    d_picker->setStateMachine( new QwtPickerDragPointMachine() );
+
+    // Включить возможность приближения/удаления графика
+    // #include <qwt_plot_magnifier.h>
+    QwtPlotMagnifier *magnifier = new QwtPlotMagnifier(d_plot->canvas());
+    // клавиша, активирующая приближение/удаление
+    magnifier->setMouseButton(Qt::MidButton);
+    // Включить возможность перемещения по графику
+    // #include <qwt_plot_panner.h>
+    QwtPlotPanner *d_panner = new QwtPlotPanner( d_plot->canvas() );
+    // клавиша, активирующая перемещение
+    d_panner->setMouseButton( Qt::RightButton );
+    // Включить отображение координат курсора и двух перпендикулярных
+    // линий в месте его отображения
+
+    QwtText* qwtt=new QwtText(title);
+    qwtt->setFont(QFont("Helvetica", 11,QFont::Normal));
+
+    d_plot->setAxisScale(1,-500,500,200);
+    d_plot->setTitle( *qwtt ); // заголовок
+    d_plot->setCanvasBackground( Qt::white ); // цвет фона
+
+
+    // Включить сетку
+    // #include <qwt_plot_grid.h>
+    //    QwtPlotGrid *grid = new QwtPlotGrid(); //
+
+    //    grid->setMajorPen(QPen( Qt::gray, 2 )); // цвет линий и толщина
+    //    grid->attach( d_plot ); // добавить сетку к полю графика
+
+
+    d_plot->setMinimumSize(150,140);
+
+}
--- dialog.h
+++ dialog.h
@@ -1,6 +1,6 @@
 #ifndef DIALOG_H
 #define DIALOG_H
-
+#include "headers.h"
 #include <QDialog>
 
 
@@ -10,6 +10,7 @@ class Dialog : public QDialog
     Q_OBJECT
 
 public:
+    void drawingInit(QwtPlot* d_plot, QString title);
     void mousePressEvent(QMouseEvent *);
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
